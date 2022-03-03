cb601023d35a487a4d1b36d5557a81b117b256d0
Bruno De Leoni <brunodeleoni@MacBook-Air.local>
2022-03-03 08:30:38 -0300

    Main Burger

--- header/header.js
+++ header/header.js
@@ -9,8 +9,17 @@ const headerComponent = (headerEl) => {
             <!-- MAIN -->
             <div class="main">
                 <!-- MAIN MOBILE -->
-                <img class="header__main-mobile-img" src="/images/img-main-burguer.png">
-                <div class="header__main-mobile"></div>
+                <img class="header__main-mobile-button" src="/images/img-main-burguer.png">
+                <div class="header__main-mobile">
+                    <div class="header__main-mobile-container-close">
+                        <button class="header__main-mobile-button-close">X</button>
+                    </div>
+                    <div class="header__main-mobile-container">
+                        <a class="header__main-mobile-item" href="/services.html">Services</a>
+                        <a class="header__main-mobile-item" href="/portfolio.html">Portfolio</a>
+                        <a class="header__main-mobile-item" href="/contact.html">Contact</a>
+                    </div>
+                </div>
                 <!-- MAIN DESKTOP -->
                 <div class="header__main-desktop">
                     <a class="header__main-desktop-item" href="/services.html">Services</a>
@@ -20,6 +29,23 @@ const headerComponent = (headerEl) => {
             </div>
         </div>
     `
+    
     headerEl.appendChild(divEl);
-};
+    
+    function clickMain (){
+        const openMainEl = document.querySelector(".header__main-mobile-button");
+        const closeMainEl = document.querySelector(".header__main-mobile-button-close");
+        const mainEl = document.querySelector(".header__main-mobile");
+    
+        openMainEl.addEventListener("click", () => {
+            mainEl.style.display = "flex";
+        })
+    
+        closeMainEl.addEventListener("click", () => {
+            mainEl.style.display = "none";
+        })
+    };
+    
+    clickMain();
 
+};
